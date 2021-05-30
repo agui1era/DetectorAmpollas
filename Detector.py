21d75331c6b7fbd2926be5e19d21a01b9d34aad1
agui1era <aguileraelectro@gmail.com>
2021-05-30 17:22:42 -0400

    file ftp

--- Detector.py
+++ Detector.py
@@ -26,7 +26,7 @@ def main():
   ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
   # force UTF-8 encoding
   ftp.encoding = "utf-8" 
-     
+
   cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
   filename =  img_input
 
@@ -69,7 +69,7 @@ def main():
       os.system('curl -v -X POST -d "{\"'+labels[obj.label_id]+'\":1}" iot.igromi.com:8080/api/v1/imagina12/telemetry --header "Content-Type:application/json"')
       with open(filename, "rb") as file:
           # use FTP's STOR command to upload the file
-          ftp.storbinary(f"STOR {output.jpg}", file)
+          ftp.storbinary(f"STOR output.jpg", file)
 
     if not objs:
       print('No objects detected.')
